a6a034b28b314009f4876e0de5622790538de9ad
PabloSA57 <camusps577@gmail.com>
2023-08-14 14:54:01 -0300

    Prueba mp

--- src/routes/mp.route.js
+++ src/routes/mp.route.js
@@ -1,22 +1,34 @@
 const express = require('express');
 
-const CartegoryService = require('./../services/category.service');
+const MercadoPagoService = require('./../services/mercadoPago.service');
 const validatorHandler = require('./../middlewares/validator.handler.js')
 
 const routerPrivate = require('../middlewares/routePrivate');
 const checkRole = require('../middlewares/role');
 
 const router = express.Router()
+const service = new MercadoPagoService()
 
-router.get('/redirect', (req, res, next) => {
-
-    console.log(req.params)
+router.get('/redirect', 
+async (req, res, next) => {
+    const { code } = req.query
     console.log(req.query)
     try {
-        res.send('MpRedirect')
+        const response = await service.redirect(code)
+        res.json(response);
     } catch (error) {
         next(error)
     }
 } )
+
+router.get('/auth', 
+async (req, res, next) => {
+    try {
+        await service.auth(res)
+    } catch (error) {
+        next(error)
+    }
+}
+)
 
 module.exports = router
--- src/services/mercadoPago.service.js
+++ src/services/mercadoPago.service.js
@@ -1,7 +1,42 @@
+require("dotenv").config();
+const axios = require('axios')
+const models = require('./../db/models/index')
+
+const { CLIENT_SECRET, CLIENT_ID, APP_ID } = process.env
+const REDIRECT_URI = "https://api-marketplace.onrender.com/api/v1/mp/redirect"
+const REDIRECT_URI_DEV = "http://localhost:3001/api/v1/mp/redirect"
+const { Token } = models
 
 
 class MercadoPagoService {
     constructor() {}
 
+    async auth(res) {
+        const authUrl = `https://auth.mercadopago.com.ar/authorization?client_id=${APP_ID}&response_type=code&platform_id=mp&redirect_uri=${REDIRECT_URI_DEV}`;
+        res.redirect(authUrl);
+    }
 
-} 
+    async redirect(code) {
+        const { data } = await axios.post('https://api.mercadopago.com/oauth/token', null, {
+            params: {
+                grant_type: 'authorization_code',
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                code,
+                redirect_uri: REDIRECT_URI,
+            },
+        });
+
+        console.log(data)
+        //await this.saveToken({access_token: data.access_token})
+
+        return {rta: 'Todo correcto'}
+    }
+
+    async saveToken(data) {
+        const token = await Token.create(data)
+
+        return token
+    }
+}
+module.exports = MercadoPagoService
