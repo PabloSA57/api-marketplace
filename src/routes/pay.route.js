000bf94f7ac1153892fe82191c719b84bfd5064b
PabloSA57 <camusps577@gmail.com>
2024-01-15 19:36:20 -0300

    prueba mp14

--- src/routes/pay.route.js
+++ src/routes/pay.route.js
@@ -14,7 +14,7 @@ router.post("/", routerPrivate, checkRole("client"), async (req, res, next) => {
   const { products, storeId } = req.body;
 
   try {
-    const response = payService.pay(products, storeId);
+    const response = await payService.pay(products, storeId);
 
     //socket.emit("notification", { list: "hola", id: socket.id });
     res.status(200).json({ init_point: response, msg: "success" });
